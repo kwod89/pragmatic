f607b28368fed77938a5b7d97d45a5f89d017b42
charlie <charlie.kwon@nitmus.com>
2022-07-08 23:33:42 +0900

    static

--- pragmatic/settings/local.py
+++ pragmatic/settings/local.py
@@ -32,13 +32,13 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'mysqldb': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pragmatic',
-        'USER': 'kwon',
-        # 'PASSWORD' : 'kwon0711',
-        'PASSWORD': 'charlie',
-        'HOST': '127.0.0.2',
-        'PORT': 3306,
-    }
+    #'mysqldb': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': 'pragmatic',
+    #    'USER': 'kwon',
+    #    # 'PASSWORD' : 'kwon0711',
+    #    'PASSWORD': 'charlie',
+    #    'HOST': '127.0.0.2',
+    #    'PORT': 3306,
+    #}
 }
